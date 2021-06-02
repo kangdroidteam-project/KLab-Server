e354ec7de5bd11d19077c6d8abcb389ba07774bf
KangDroid <hyunwoo286@gmail.com>
2021-06-02 14:52:31 +0900

    Repository: Add findUserById

--- src/main/kotlin/com/branch/server/data/user/UserRepository.kt
+++ src/main/kotlin/com/branch/server/data/user/UserRepository.kt
@@ -1,11 +1,36 @@
 package com.branch.server.data.user
 
+import com.branch.server.error.exception.NotFoundException
+import com.branch.server.error.exception.UnknownErrorException
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.find
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 class UserRepository(
     private val mongoTemplate: MongoTemplate
 ) {
+    // Field Declare
+    private val userIdField: String = "userId"
+
     fun addUser(user: User): User = mongoTemplate.save(user)
+
+    fun findUserById(userId: String): User {
+        val userList: List<User> = mongoTemplate.find(
+            Query(Criteria.where(userIdField).`is`(userId))
+        )
+
+        // Ensure user list contains 'exactly' one.
+        ensureUserListContainsOne(userList)
+
+        return userList[0]
+    }
+
+    private fun ensureUserListContainsOne(userList: List<User>) {
+        if (userList.size != 1) {
+            throw UnknownErrorException("User find result is not exactly 1!")
+        }
+    }
 }
--- src/test/kotlin/com/branch/server/data/user/UserRepositoryTest.kt
+++ src/test/kotlin/com/branch/server/data/user/UserRepositoryTest.kt
@@ -1,12 +1,18 @@
 package com.branch.server.data.user
 
+import com.branch.server.error.exception.NotFoundException
+import com.branch.server.error.exception.UnknownErrorException
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.fail
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.findAll
+import org.springframework.data.mongodb.core.query.Query
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @SpringBootTest
@@ -17,6 +23,20 @@ internal class UserRepositoryTest {
 
     @Autowired
     private lateinit var mongoTemplate: MongoTemplate
+
+    private val mockUser: User = User(
+        userId = "kangdroid",
+        userPassword = "test",
+        userName = "KangDroid",
+        userAddress = "test",
+        userPhoneNumber = "test"
+    )
+
+    @BeforeEach
+    @AfterEach
+    fun initTest() {
+        mongoTemplate.remove(Query(), User::class.java)
+    }
 
     @Test
     fun is_addUser_works_well() {
@@ -36,4 +56,30 @@ internal class UserRepositoryTest {
         assertThat(userList.size).isEqualTo(1)
         assertThat(userList[0].userId).isEqualTo(mockUser.userId)
     }
+
+    @Test
+    fun is_findUserById_works_well() {
+        // Save
+        userRepository.addUser(mockUser)
+        runCatching {
+            userRepository.findUserById(mockUser.userId)
+        }.onSuccess {
+            assertThat(it.userId).isEqualTo(mockUser.userId)
+        }.onFailure {
+            println(it.stackTraceToString())
+            fail("User data is set but it failed")
+        }
+    }
+
+    @Test
+    fun is_findUserById_throws_unknownError() {
+        runCatching {
+            userRepository.findUserById(mockUser.userId)
+        }.onSuccess {
+            fail("We do not have entity but it succeed?")
+        }.onFailure {
+            assertThat(it is UnknownErrorException).isEqualTo(true)
+        }
+    }
+
 }
